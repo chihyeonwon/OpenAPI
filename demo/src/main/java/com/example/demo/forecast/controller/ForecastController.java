f049c211864cb2c14fd85ae36dfa52e92c019da0
ylose <heungeob1003@gmail.com>
2024-01-21 18:49:48 +0900

    Feat : Deserializer annotation

---
+++ demo/src/main/java/com/example/demo/forecast/controller/ForecastController.java
@@ -0,0 +1,102 @@
+package com.example.demo.forecast.controller;
+
+import com.example.demo.forecast.dto.FcstItems;
+import com.example.demo.forecast.service.ForecastService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class ForecastController {
+    @Value("${openApi.serviceKey}")
+    private String serviceKey;
+
+    @Value("${openApi.callBackUrl}")
+    private String callBackUrl;
+
+    @Value("${openApi.dataType}")
+    private String dataType;
+
+    private final ForecastService forecastService;
+
+    @GetMapping("/forecast")
+    public ResponseEntity<FcstItems> callForecastApi(
+            @RequestParam(value="base_time") String baseTime,
+            @RequestParam(value="base_date") String baseDate,
+            @RequestParam(value="beach_num") String beachNum
+    ){
+        HttpURLConnection urlConnection = null;
+        InputStream stream = null;
+        String result = null;
+
+        String urlStr = callBackUrl +
+                "serviceKey=" + serviceKey +
+                "&dataType=" + dataType +
+                "&base_date=" + baseDate +
+                "&base_time=" + baseTime +
+                "&beach_num=" + beachNum;
+
+        try {
+            URL url = new URL(urlStr);
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            stream = getNetworkConnection(urlConnection);
+            result = readStreamToString(stream);
+
+            if (stream != null) stream.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+
+        FcstItems response = forecastService.parsingJsonObject(result);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    /* URLConnection 을 전달받아 연결정보 설정 후 연결, 연결 후 수신한 InputStream 반환 */
+    private InputStream getNetworkConnection(HttpURLConnection urlConnection) throws IOException {
+        urlConnection.setConnectTimeout(3000);
+        urlConnection.setReadTimeout(3000);
+        urlConnection.setRequestMethod("GET");
+        urlConnection.setDoInput(true);
+
+        if(urlConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            throw new IOException("HTTP error code : " + urlConnection.getResponseCode());
+        }
+
+        return urlConnection.getInputStream();
+    }
+
+    /* InputStream을 전달받아 문자열로 변환 후 반환 */
+    private String readStreamToString(InputStream stream) throws IOException{
+        StringBuilder result = new StringBuilder();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+
+        String readLine;
+        while((readLine = br.readLine()) != null) {
+            result.append(readLine + "\n\r");
+        }
+
+        br.close();
+
+        return result.toString();
+    }
+}
